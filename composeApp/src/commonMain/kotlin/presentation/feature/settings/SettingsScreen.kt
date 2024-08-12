f077a06813ae13a7d999e746051cbdf0206fa332
o.bakhur <o.bakhur@vironit.co.uk>
2024-08-12 13:14:37 +0200

    Add EditThemeBottomSheet

--- composeApp/src/commonMain/kotlin/presentation/feature/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/feature/settings/SettingsScreen.kt
@@ -8,30 +8,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Badge
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import newsappkmp.composeapp.generated.resources.Res
 import newsappkmp.composeapp.generated.resources.contact_us
 import newsappkmp.composeapp.generated.resources.inbox
+import newsappkmp.composeapp.generated.resources.save
 import newsappkmp.composeapp.generated.resources.screen_title_settings
 import newsappkmp.composeapp.generated.resources.theme
 import org.jetbrains.compose.resources.stringResource
@@ -46,7 +58,6 @@ fun SettingsScreen(
     viewModel: SettingsViewModel = koinViewModel(),
     onBackClicked: () -> Unit
 ) {
-
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     ScreenContent(
@@ -87,19 +98,32 @@ private fun ScreenContent(
         }
     ) { paddingValues ->
         /* Test value */
-        var itemCount by rememberSaveable { mutableStateOf(20) }
+        var inboxCount by rememberSaveable { mutableStateOf(20) }
+
+        val scope = rememberCoroutineScope()
+        val editThemeBottomSheetState = rememberModalBottomSheetState()
+        var showEditThemeBottomSheet by rememberSaveable { mutableStateOf(false) }
 
         ScreenContent(
-            itemCount = itemCount,
-            paddingValues = paddingValues
+            paddingValues = paddingValues,
+            inboxCount = inboxCount,
+            scope = scope,
+            showEditThemeBottomSheet = showEditThemeBottomSheet,
+            editThemeBottomSheetState = editThemeBottomSheetState,
+            onShowBottomSheetEditThemeChanged = { showEditThemeBottomSheet = it }
         )
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(
-    itemCount: Int,
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    inboxCount: Int,
+    scope: CoroutineScope,
+    showEditThemeBottomSheet: Boolean,
+    editThemeBottomSheetState: SheetState,
+    onShowBottomSheetEditThemeChanged: (Boolean) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -121,12 +145,12 @@ fun ScreenContent(
                 }
             )
 
-            if (itemCount > 0) {
+            if (inboxCount > 0) {
                 Badge(
                     containerColor = Color.Red,
                     contentColor = Color.White
                 ) {
-                    Text("$itemCount")
+                    Text("$inboxCount")
                 }
             }
         }
@@ -138,7 +162,7 @@ fun ScreenContent(
             icon = Icons.Filled.Edit,
             contentDescription = stringResource(Res.string.theme),
             onClick = {
-                // TODO
+                onShowBottomSheetEditThemeChanged.invoke(true)
             }
         )
 
@@ -152,5 +176,56 @@ fun ScreenContent(
                 // TODO
             }
         )
+
+        ShowBottomSheetEditTheme(
+            scope = scope,
+            showEditThemeBottomSheet = showEditThemeBottomSheet,
+            editThemeBottomSheetState = editThemeBottomSheetState,
+            onShowBottomSheetEditThemeChanged = onShowBottomSheetEditThemeChanged
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowBottomSheetEditTheme(
+    scope: CoroutineScope,
+    showEditThemeBottomSheet: Boolean,
+    editThemeBottomSheetState: SheetState,
+    onShowBottomSheetEditThemeChanged: (Boolean) -> Unit
+) {
+    if (showEditThemeBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                onShowBottomSheetEditThemeChanged.invoke(false)
+            },
+            sheetState = editThemeBottomSheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+            ) {
+                Button(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally),
+                    onClick = {
+                        scope.launch { editThemeBottomSheetState.hide() }.invokeOnCompletion {
+                            if (!editThemeBottomSheetState.isVisible) {
+                                onShowBottomSheetEditThemeChanged.invoke(false)
+                            }
+                        }
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(end = 8.dp),
+                        imageVector = Icons.Default.Done,
+                        contentDescription = stringResource(Res.string.save),
+                    )
+                    Text(text = stringResource(Res.string.save))
+                }
+            }
+        }
     }
 }

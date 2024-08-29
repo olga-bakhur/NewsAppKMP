b2be80a6a47458a5b51e5242a5b5a1af6360b5f9
o.bakhur <o.bakhur@vironit.co.uk>
2024-08-29 15:06:55 +0200

    Add navigation animation

--- composeApp/src/commonMain/kotlin/presentation/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/presentation/navigation/AppNavHost.kt
@@ -1,10 +1,20 @@
 package presentation.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -20,6 +30,8 @@ import presentation.feature.favorite.FavoriteScreen
 import presentation.feature.feed.FeedScreen
 import presentation.feature.profile.ProfileScreen
 import presentation.feature.settings.SettingsScreen
+
+private const val animationDuration = 400
 
 @Composable
 fun AppNavHost(
@@ -50,7 +62,13 @@ fun NavGraphBuilder.navigationBarGraph(
         route = Graph.NavigationBar.route
     ) {
         /* tabs */
-        composable(Screen.Feed.route) {
+        composable(
+            route = Screen.Feed.route,
+            enterTransition = { enterAnimation() },
+            exitTransition = { exitAnimation() },
+            popEnterTransition = { enterAnimation() },
+            popExitTransition = { exitAnimation() }
+        ) {
             FeedScreen(
                 viewModel = koinViewModel(),
                 onArticleClicked = { articleId ->
@@ -61,21 +79,39 @@ fun NavGraphBuilder.navigationBarGraph(
             )
         }
 
-        composable(Screen.Favorite.route) {
+        composable(
+            route = Screen.Favorite.route,
+            enterTransition = { enterAnimation() },
+            exitTransition = { exitAnimation() },
+            popEnterTransition = { enterAnimation() },
+            popExitTransition = { exitAnimation() }
+        ) {
             FavoriteScreen(
                 viewModel = koinViewModel(),
                 onBackClicked = { navController.popBackStack() }
             )
         }
 
-        composable(Screen.Profile.route) {
+        composable(
+            route = Screen.Profile.route,
+            enterTransition = { enterAnimation() },
+            exitTransition = { exitAnimation() },
+            popEnterTransition = { enterAnimation() },
+            popExitTransition = { exitAnimation() }
+        ) {
             ProfileScreen(
                 viewModel = koinViewModel(),
                 onBackClicked = { navController.popBackStack() }
             )
         }
 
-        composable(Screen.Settings.route) {
+        composable(
+            route = Screen.Settings.route,
+            enterTransition = { enterAnimation() },
+            exitTransition = { exitAnimation() },
+            popEnterTransition = { enterAnimation() },
+            popExitTransition = { exitAnimation() }
+        ) {
             SettingsScreen(
                 viewModel = koinViewModel(),
                 onBackClicked = { navController.popBackStack() }
@@ -90,7 +126,11 @@ fun NavGraphBuilder.navigationBarGraph(
                     type = NavType.StringType
                     defaultValue = EMPTY
                 }
-            )
+            ),
+            enterTransition = { enterAnimation() },
+            exitTransition = { exitAnimation() },
+            popEnterTransition = { enterAnimation() },
+            popExitTransition = { exitAnimation() }
         ) { backStackEntry ->
             ArticleDetailScreen(
                 viewModel = koinViewModel(),
@@ -100,3 +140,31 @@ fun NavGraphBuilder.navigationBarGraph(
         }
     }
 }
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.enterAnimation(): EnterTransition =
+    fadeIn(
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            easing = LinearEasing
+        )
+    ) + slideIntoContainer(
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            easing = EaseIn
+        ),
+        towards = AnimatedContentTransitionScope.SlideDirection.Start
+    )
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.exitAnimation(): ExitTransition =
+    fadeOut(
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            easing = LinearEasing
+        )
+    ) + slideOutOfContainer(
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            easing = EaseOut
+        ),
+        towards = AnimatedContentTransitionScope.SlideDirection.End
+    )

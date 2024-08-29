b8a46cf3b7891762861eb2e0a568540a987915d3
o.bakhur <o.bakhur@vironit.co.uk>
2024-08-29 12:20:16 +0200

    Refactoring UI

--- composeApp/src/commonMain/kotlin/presentation/component/BaseCard.kt
+++ composeApp/src/commonMain/kotlin/presentation/component/BaseCard.kt
@@ -1,8 +1,6 @@
 package presentation.component
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
@@ -13,24 +11,18 @@ import presentation.theme.Theme
 @Composable
 fun BaseCard(
     modifier: Modifier = Modifier,
-    contentPadding: Dp = Theme.dimens.space16,
     elevation: Dp = Theme.dimens.space6,
     onCardClicked: (() -> Unit)? = null,
     content: @Composable() (ColumnScope.() -> Unit)
 ) {
     ElevatedCard(
+        modifier = modifier,
         elevation = CardDefaults.cardElevation(
             defaultElevation = elevation
         ),
         onClick = {
             onCardClicked?.invoke()
         },
-        content = {
-            Column(
-                modifier = modifier.padding(contentPadding)
-            ) {
-                content.invoke(this@ElevatedCard)
-            }
-        }
+        content = content
     )
 }
--- composeApp/src/commonMain/kotlin/presentation/feature/articledetail/component/ArticleDetailEmptyState.kt
+++ composeApp/src/commonMain/kotlin/presentation/feature/articledetail/component/ArticleDetailEmptyState.kt
@@ -1,8 +1,10 @@
 package presentation.feature.articledetail.component
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import newsappkmp.composeapp.generated.resources.Res
 import newsappkmp.composeapp.generated.resources.no_info
@@ -13,11 +15,11 @@ fun ArticleDetailEmptyState(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
         Text(
-            text = stringResource(Res.string.no_info),
-            modifier = modifier
+            text = stringResource(Res.string.no_info)
         )
     }
 }
